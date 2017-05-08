84b27d77d3366700ae6eef3e8704ca26fd01b3b7
Colin Panisset <colin.panisset@cevo.com.au>
2017-05-08 10:59:51 +1000

    Capture implicit dependency on google api client

--- setup.py
+++ setup.py
@@ -78,6 +78,7 @@ setup(
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     # install_requires=['peppercorn'],
+    install_requires=['google-api-python-client'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
